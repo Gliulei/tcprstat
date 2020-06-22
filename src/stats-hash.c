091cee6a75eac7a9c67df7e806cae08e214b4e8a
liulei <1245332635@qq.com>
2020-06-22 17:28:29 +0800

    tcp

--- src/stats-hash.c
+++ src/stats-hash.c
@@ -69,7 +69,7 @@ hash_new(void) { //��¼Դ�˿� ԴIP  Ŀ�Ķ˿�  Ŀ��IP��hash�
     // Don't change following ret->sz for initial_hash_sz. That wouldn't be
     // very thread_safe (not that the whole module is :)
     //ret->sessions = malloc(ret->sz * sizeof(struct session));
-    ret->sessions = malloc(ret->sz * (sizeof(struct session) + sizeof(char)));
+    ret->sessions = malloc(ret->sz * (sizeof(struct session) + 100*sizeof(char)));
     if (!ret->sessions)
         abort();
     memset(ret->sessions, 0, ret->sz * sizeof(struct session));
