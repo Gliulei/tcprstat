c7aa26ba067b1ebc796aed4b25a82d962068cb9f
liulei <1245332635@qq.com>
2020-06-26 22:05:26 +0800

    tcp

--- src/stats.c
+++ src/stats.c
@@ -189,8 +189,8 @@ outbound(struct timeval tv, char* data, struct in_addr laddr, struct in_addr rad
 {
     struct timeval start;
     unsigned long newstat;
-    //char res[1024] = {'\0'};
-    char* res = NULL;
+    char res[1024] = {'\0'};
+    //char* res = NULL;
     // int n = 0;
     char* key = NULL;
     
